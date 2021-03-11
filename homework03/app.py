99a079148fa3391060ae3fd1cd1ba0f3c50fe1ce
Cameron Cummins <cameronscummins@gmail.com>
2021-03-10 19:22:43 -0600

    Added routes

--- homework03/app.py
+++ homework03/app.py
@@ -3,10 +3,19 @@ import json
 app = Flask(__name__)
 
 @app.route('/animals', methods=['GET'])
-def getAnimal():
+def getAnimals():
     with open("animals.json", "r") as json_file:
         userdata = json.load(json_file)
-    return str(userdata)
+    return json.dumps(userdata, sort_keys = True, indent = 4)
+
+@app.route('/animals/<string:body_part>/<string:var>', methods=['GET'])
+def filterAnimals(body_part, var):
+    ret_animals = []
+    with open("animals.json", "r") as json_file:
+        for animal in json.load(json_file):
+            if animal[body_part] == var:
+                ret_animals.append(animal)
+    return str(ret_animals)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
