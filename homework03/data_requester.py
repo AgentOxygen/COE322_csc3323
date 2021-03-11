0ebac5f40b70d199346915de8adfb4cb392ae716
Cameron Cummins <cameronscummins@gmail.com>
2021-03-10 19:33:41 -0600

    Fixed error again lol

--- homework03/data_requester.py
+++ homework03/data_requester.py
@@ -9,7 +9,7 @@ import requests
 import sys
 
 if __name__ == '__main__':
-    response = requests.get(url=sys.argv)
+    response = requests.get(url=sys.argv[1])
     print(response.status_code)
     print(response.json())
     print(response.headers)
